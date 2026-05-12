4c0a5b243182dfc626621d87b8f5e84bc2b516bc
Matt Calvert <83599413+awesomebunny2@users.noreply.github.com>
2026-05-12 12:23:41 -0400

    all caught up now

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,17 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/components/**/*.stories.@(ts|tsx)'],
-  staticDirs: ['../public'],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-vitest',
-    '@chromatic-com/storybook',
-  ],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
+    stories: ['../src/components/**/*.stories.@(ts|tsx)'],
+    staticDirs: ['../public'],
+    addons: [
+        '@storybook/addon-docs',
+        '@storybook/addon-vitest',
+        '@chromatic-com/storybook',
+    ],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {},
+    },
 };
 
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,17 +1,17 @@
-/// <reference types="vite/client" />
+  /// <reference types="vite/client" />
 import type { Preview } from '@storybook/react-vite';
 
 import '../src/index.css';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
+    parameters: {
+      controls: {
+        matchers: {
+          color: /(background|color)$/i,
+          date: /Date$/,
+        },
       },
     },
-  },
 };
 
 export default preview;
